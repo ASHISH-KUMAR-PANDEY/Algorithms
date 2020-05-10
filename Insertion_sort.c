9d77bd909d7d32ce6b46d602b800b3e3e02bc4b2
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-11 00:16:11 +0530

    Update Insertion_sort.c

--- Insertion_sort.c
+++ Insertion_sort.c
@@ -4,3 +4,7 @@ int main(){
 
    printf("How many numbers u are going to enter?: ");
    scanf("%d",&count);
+printf("Enter %d elements: ", count);
+   // This loop would store the input numbers in array
+   for(i=0;i<count;i++)
+      scanf("%d",&number[i]);

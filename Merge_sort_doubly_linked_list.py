b487427caeb0b5eae9d9ef2818de1194cd4cc51f
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-18 12:34:29 +0530

    Update Merge_sort_doubly_linked_list.py

--- Merge_sort_doubly_linked_list.py
+++ Merge_sort_doubly_linked_list.py
@@ -13,8 +13,6 @@ class DoublyLinkedList:
             return second  
         if second is None: 
             return first 
-  
-        # Pick the smaller value 
         if first.data < second.data: 
             first.next = self.merge(first.next, second) 
             first.next.prev = first 
@@ -25,25 +23,17 @@ class DoublyLinkedList:
             second.next.prev = second 
             second.prev = None
             return second 
-  
-    # Function to do merge sort 
     def mergeSort(self, tempHead): 
         if tempHead is None:  
             return tempHead 
         if tempHead.next is None: 
             return tempHead 
-          
-        second = self.split(tempHead) 
-          
-        # Recur for left and righ halves 
+        
         tempHead = self.mergeSort(tempHead) 
         second = self.mergeSort(second) 
-  
-        # Merge the two sorted halves 
+
         return self.merge(tempHead, second) 
-  
-    # Split the doubly linked list (DLL) into two DLLs 
-    # of half sizes 
+
     def split(self, tempHead): 
         fast = slow =  tempHead 
         while(True): 
@@ -57,25 +47,16 @@ class DoublyLinkedList:
         temp = slow.next
         slow.next = None
         return temp 
-          
-              
-    # Given a reference to the head of a list and an 
-    # integer,inserts a new node on the front of list 
+
     def push(self, new_data): 
-   
-        # 1. Allocates node 
-        # 2. Put the data in it 
+
         new_node = Node(new_data) 
-   
-        # 3. Make next of new node as head and 
-        # previous as None (already None) 
+ 
         new_node.next = self.head 
-   
-        # 4. change prev of head node to new_node 
+
         if self.head is not None: 
             self.head.prev = new_node 
-   
-        # 5. move the head to point to the new node 
+
         self.head = new_node 
   
   
@@ -90,8 +71,7 @@ class DoublyLinkedList:
         while(temp): 
             print temp.data, 
             temp = temp.prev 
-  
-# Driver program to test the above functions 
+
 dll = DoublyLinkedList() 
 dll.push(5) 
 dll.push(20); 
@@ -102,4 +82,3 @@ dll.push(10);
 dll.head = dll.mergeSort(dll.head)    
 print "Linked List after sorting"
 dll.printList(dll.head) 
-  

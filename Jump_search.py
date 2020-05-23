4fc6103c0f87efe06b0bd72d2cdd32601841432a
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-23 19:57:19 +0530

    Update Jump_search.py

--- Jump_search.py
+++ Jump_search.py
@@ -8,31 +8,17 @@ def jumpSearch( arr , x , n ):
         step += math.sqrt(n) 
         if prev >= n: 
             return -1
-      
-    # Doing a linear search for x in  
-    # block beginning with prev. 
     while arr[int(prev)] < x: 
-        prev += 1
-          
-        # If we reached next block or end  
-        # of array, element is not present. 
+        prev += 1 
         if prev == min(step, n): 
             return -1
-      
-    # If element is found 
     if arr[int(prev)] == x: 
         return prev 
       
     return -1
-  
-# Driver code to test function 
 arr = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 
     34, 55, 89, 144, 233, 377, 610 ] 
 x = 55
 n = len(arr) 
-  
-# Find the index of 'x' using Jump Search 
 index = jumpSearch(arr, x, n) 
-  
-# Print the index where 'x' is located 
 print("Number" , x, "is at index" ,"%.0f"%index)

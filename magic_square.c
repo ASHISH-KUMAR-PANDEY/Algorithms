075582492d5dd971a0415b6eac76e59cb7af76e7
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-05-25 20:39:41 +0530

    Update magic_square.c

--- magic_square.c
+++ magic_square.c
@@ -1,30 +1,20 @@
 #include<stdio.h> 
 #include<string.h> 
-  
-// A function to generate odd sized magic squares 
 void generateSquare(int n) 
 { 
     int magicSquare[n][n]; 
-  
-    // set all slots as 0 
     memset(magicSquare, 0, sizeof(magicSquare)); 
-  
-    // Initialize position for 1 
     int i = n/2; 
     int j = n-1; 
-  
-    // One by one put all values in magic square 
     for (int num=1; num <= n*n; ) 
     { 
-        if (i==-1 && j==n) //3rd condition 
+        if (i==-1 && j==n)
         { 
             j = n-2; 
             i = 0; 
         } 
         else
         { 
-            // 1st condition helper if next number  
-            // goes to out of square's right side 
             if (j == n) 
                 j = 0; 
   

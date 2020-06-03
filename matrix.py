418296b13542a4100b797261a8af4b9cb63a565b
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-06-03 23:08:12 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,24 +1,16 @@
-import math as mt 
-  
-# Size of given matrix is N X N 
+import math as mt  
 N = 6
-  
-# A utility function to find minimum 
-# of two numbers 
+
 def getMin(x, y): 
     if x < y: 
         return x 
     else: 
         return y 
-          
-# Returns size of Maximum size  
-# subsquare matrix surrounded by 'X' 
+
 def findSubSquare(mat): 
   
-    Max = 0 # Initialize result 
-  
-    # Initialize the left-top value  
-    # in hor[][] and ver[][] 
+    Max = 0 
+ 
     hor = [[0 for i in range(N)]  
               for i in range(N)] 
     ver = [[0 for i in range(N)]  
@@ -27,8 +19,7 @@ def findSubSquare(mat):
     if mat[0][0] == 'X': 
         hor[0][0] = 1
         ver[0][0] = 1
-  
-    # Fill values in hor[][] and ver[][] 
+
     for i in range(N): 
       
         for j in range(N): 
@@ -42,30 +33,13 @@ def findSubSquare(mat):
                     (ver[i][j],  
                      hor[i][j]) = (ver[i - 1][j] + 1,  
                                    hor[i][j - 1] + 1) 
-  
-    # Start from the rightmost-bottommost corner  
-    # element and find the largest ssubsquare 
-    # with the help of hor[][] and ver[][] 
+
     for i in range(N - 1, 0, -1): 
       
         for j in range(N - 1, 0, -1): 
-          
-            # Find smaller of values in hor[][] and  
-            # ver[][]. A Square can only be made by  
-            # taking smaller value 
+
             small = getMin(hor[i][j], ver[i][j]) 
   
-            # At this point, we are sure that there  
-            # is a right vertical line and bottom  
-            # horizontal line of length at least 'small'. 
-  
-            # We found a bigger square if following  
-            # conditions are met: 
-            # 1)If side of square is greater than Max. 
-            # 2)There is a left vertical line  
-            #   of length >= 'small' 
-            # 3)There is a top horizontal line  
-            #   of length >= 'small' 
             while (small > Max): 
               
                 if (ver[i][j - small + 1] >= small and 
@@ -77,7 +51,6 @@ def findSubSquare(mat):
               
     return Max
   
-# Driver Code 
 mat = [['X', 'O', 'X', 'X', 'X', 'X'], 
        ['X', 'O', 'X', 'X', 'O', 'X'], 
        ['X', 'X', 'X', 'O', 'O', 'X'], 

e0a72d6c9f78032945c86f35d4fbdad01dce1b0a
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-06-12 19:21:58 +0530

    Update wrapping_algorithm.py

--- wrapping_algorithm.py
+++ wrapping_algorithm.py
@@ -40,40 +40,23 @@ def convexHull(points, n):
     while(True): 
  
         hull.append(p) 
-  
-        ''' 
-        Search for a point 'q' such that orientation(p, x,  
-        q) is counterclockwise for all points 'x'. The idea  
-        is to keep track of last visited most counterclock-  
-        wise point in q. If any point 'i' is more counterclock-  
-        wise than q, then update q.  
-        '''
+
         q = (p + 1) % n 
   
         for i in range(n): 
-              
-            # If i is more counterclockwise  
-            # than current q, then update q  
+
             if(orientation(points[p],  
                            points[i], points[q]) == 2): 
                 q = i 
-  
-        ''' 
-        Now q is the most counterclockwise with respect to p  
-        Set p as q for next iteration, so that q is added to  
-        result 'hull'  
-        '''
+
         p = q 
-  
-        # While we don't come to first point 
+
         if(p == l): 
             break
-  
-    # Print Result  
+
     for each in hull: 
         print(points[each].x, points[each].y) 
-  
-# Driver Code 
+
 points = [] 
 points.append(Point(0, 3)) 
 points.append(Point(2, 2)) 

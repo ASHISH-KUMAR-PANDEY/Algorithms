2088ff0353579c05118766e23bdb6a4a67ead1c2
Ashish Pandey <ashishkumarpandey2000@gmail.com>
2020-06-22 20:09:08 +0530

    Update dijikstra.py

--- dijikstra.py
+++ dijikstra.py
@@ -11,13 +11,9 @@ class Graph():
         print "Vertex \tDistance from Source"
         for node in range(self.V): 
             print node, "\t", dist[node] 
-  
-    # A utility function to find the vertex with  
-    # minimum distance value, from the set of vertices  
-    # not yet included in shortest path tree 
+
     def minDistance(self, dist, sptSet): 
-  
-        # Initilaize minimum distance for next node 
+
         min = sys.maxint 
   
         # Search not nearest vertex not in the  
@@ -28,10 +24,7 @@ class Graph():
                 min_index = v 
   
         return min_index 
-  
-    # Funtion that implements Dijkstra's single source  
-    # shortest path algorithm for a graph represented  
-    # using adjacency matrix representation 
+
     def dijkstra(self, src): 
   
         dist = [sys.maxint] * self.V 
@@ -39,28 +32,18 @@ class Graph():
         sptSet = [False] * self.V 
   
         for cout in range(self.V): 
-  
-            # Pick the minimum distance vertex from  
-            # the set of vertices not yet processed.  
-            # u is always equal to src in first iteration 
+
             u = self.minDistance(dist, sptSet) 
-  
-            # Put the minimum distance vertex in the  
-            # shotest path tree 
+
             sptSet[u] = True
   
-            # Update dist value of the adjacent vertices  
-            # of the picked vertex only if the current  
-            # distance is greater than new distance and 
-            # the vertex in not in the shotest path tree 
             for v in range(self.V): 
                 if self.graph[u][v] > 0 and sptSet[v] == False and \ 
                 dist[v] > dist[u] + self.graph[u][v]: 
                         dist[v] = dist[u] + self.graph[u][v] 
   
         self.printSolution(dist) 
-  
-# Driver program 
+
 g = Graph(9) 
 g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0], 
         [4, 0, 8, 0, 0, 0, 0, 11, 0], 
